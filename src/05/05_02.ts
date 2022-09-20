4b6bd4f6b63283581479b065a560ac16c4404519
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-09-20 19:44:13 +0300

    map

---
+++ src/05/05_02.ts
@@ -0,0 +1,14 @@
+import {GovernmentBuildingType, HouseType} from "../02/02_02";
+
+export const getStreetsTitlesOfGovernmentBuildings =
+    (buildings: Array<GovernmentBuildingType>) => {
+        return buildings.map(b => b.address.street.title)
+    }
+
+export const getStreetsTitlesOfHouses = (houses: Array<HouseType>) => {
+    return houses.map(s => s.address.street.title)
+}
+
+export const createMessages = (houses: Array<HouseType>) => {
+    return houses.map(m => "Hello guys from " + m.address.street.title)
+}
--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = ["Lyuda", "Katya", "Dima", "Viktor"]
+  const users = [{id: 1, name: "Lyuda"}, {id: 2, name: "Katya"},{id:3, name: "Dima"},{id:4, name: "Viktor"}]
+
+
+const liElements = users.map(u => <li key={u.id}>{u.name}</li>)
+
   return (
     <div className="App">
-
+      <ul>
+        {liElements}
+      </ul>
     </div>
   );
 }
