144793b02ea18944443a1a69e0bd06edc55a3320
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-09-20 13:21:13 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWorlds} from "./01-hello-tests/01";
+
+const sentense ="Hello my friends";
+const result = splitIntoWorlds(sentense)
+console.log(result[0] === "hello")
+console.log(result[1] === "my")
+console.log(result[2] === "friend")
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
